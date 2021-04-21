f49f33139d1f190632c910cb5b2105a588773b6b
17thaugust <78376364+17thaugust@users.noreply.github.com>
2021-04-21 21:39:11 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
         Coded by {" "}
       <a href="https://github.com/17thaugust/weather-app-react" target="_blank">Karolina</a>
     </footer>
+    </div>
     </div>
   );
 }
