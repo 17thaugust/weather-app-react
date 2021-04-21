9d6e832adafacff9b58137e59ec92daa7654c574
17thaugust <78376364+17thaugust@users.noreply.github.com>
2021-04-21 21:19:38 +0200

    Added GitHub repository link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        Coded by {" "}
+      <a href="https://github.com/17thaugust/weather-app-react" target="_blank">Karolina</a>
+    </footer>
     </div>
   );
 }
