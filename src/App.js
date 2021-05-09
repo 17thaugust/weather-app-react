7f88edbddad69d52097a0d795b7ab4840ff91ba4
17thaugust <78376364+17thaugust@users.noreply.github.com>
2021-05-09 13:25:50 +0200

    Added refreshing forecast

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather defaultCity="Lisbon" />
+      <Weather defaultCity="Warsaw" />
     
       <footer>
         Coded by {" "}
